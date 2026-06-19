e121a6424ebebef06b2313b68493e129295cf6a8
Brigitte Rodriguez <barodriguez7@espe.edu.ec>
2026-06-19 07:57:00 -0500

    refactor(backend): establecer paquete principal del proyecto

---
+++ src/main/java/com/movsapp/backend/MovsAppBackendApplication.java
@@ -0,0 +1,11 @@
+package com.movsapp.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovsAppBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MovsAppBackendApplication.class, args);
+    }
+}
