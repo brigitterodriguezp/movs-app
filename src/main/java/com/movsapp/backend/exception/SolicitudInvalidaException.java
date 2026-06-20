bf77dba51bd83133ff1604e07bd6e5177dae4138
Brigitte Rodriguez <barodriguez7@espe.edu.ec>
2026-06-20 11:58:00 -0500

    feat(exception): agregar error de solicitud invalida

---
+++ src/main/java/com/movsapp/backend/exception/SolicitudInvalidaException.java
@@ -0,0 +1,4 @@
+package com.movsapp.backend.exception;
+public class SolicitudInvalidaException extends RuntimeException {
+    public SolicitudInvalidaException(String message) { super(message); }
+}
