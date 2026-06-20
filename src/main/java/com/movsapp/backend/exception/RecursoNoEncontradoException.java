acb824dfac0d613289a187cdecc6d733d89e2040
Brigitte Rodriguez <barodriguez7@espe.edu.ec>
2026-06-20 09:04:00 -0500

    feat(exception): agregar error de recurso no encontrado

---
+++ src/main/java/com/movsapp/backend/exception/RecursoNoEncontradoException.java
@@ -0,0 +1,4 @@
+package com.movsapp.backend.exception;
+public class RecursoNoEncontradoException extends RuntimeException {
+    public RecursoNoEncontradoException(String message) { super(message); }
+}
