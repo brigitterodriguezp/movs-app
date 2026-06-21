2c3f3cfecff56682c4729baa6c8e535ce2e3643a
Brigitte Rodriguez <barodriguez7@espe.edu.ec>
2026-06-21 10:22:21 -0500

    commit inicial, previo a la implementacion del backend

---
+++ src/test/java/com/app/movs/MovsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.movs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
